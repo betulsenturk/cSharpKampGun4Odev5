c79c241e4448c0c48945629dd7650896fb33eeed
Betül Şentürk <betul_senturk2000@hotmail.com>
2022-06-03 14:35:21 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun4Odev5
+{
+    class MyDictionary<K, V>
+    {
+        public K[] keys;
+        public V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            Console.WriteLine("'" + key + ": " + value + "' eklendi.");
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("\n+LISTE+");
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ": " + values[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gun4Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> users = new MyDictionary<int, string>();
+
+            users.Add(2, "Merve");
+            users.Add(5, "Nurcan");
+            users.Add(3, "Betül");
+
+            users.Print();
+        }
+    }
+}
